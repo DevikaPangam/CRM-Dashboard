23912f68287e9aa6ce22bf82bf263b595a87c7ae
DevikaPangam <devika.p@rajmudragroup.com>
2026-09-18 18:02:23 +0530

    fix: explicit handler wrapper in api/index.js for Vercel

--- api/index.js
+++ api/index.js
@@ -1,3 +1,14 @@
 const app = require('../server.js');
 
-module.exports = app;
+module.exports = (req, res) => {
+  try {
+    return app(req, res);
+  } catch (err) {
+    console.error('Vercel serverless function error:', err);
+    res.status(500).json({
+      error: 'SERVERLESS_FUNCTION_ERROR',
+      message: err.message,
+      stack: err.stack
+    });
+  }
+};
